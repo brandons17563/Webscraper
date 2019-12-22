3c5428b982421f882ecc762b4c3c81aba7e7d137
brandons17563 <46284783+brandons17563@users.noreply.github.com>
2019-12-22 10:52:14 -0600

    initial web scraper files

---
+++ parsedata.py
@@ -0,0 +1,16 @@
+# Author: Brandon Schenck
+# Date: 12/18/2019
+# Description:
+
+import json
+def main():
+    fp = open("scraped_data.json","r")
+    fw = open("pretty_data.txt","w")
+
+
+
+
+
+
+    fp.close()
+    fw.close()
